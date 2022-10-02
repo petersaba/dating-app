507d3fd73a4707337e523ca9467754be890ac7a8
Peter Saba <petersaba001@gmail.com>
2022-10-02 23:40:38 +0300

    frontend <> js utilities: added check for strong password

--- frontend/javascript/utilities.js
+++ frontend/javascript/utilities.js
@@ -1,12 +1,30 @@
 const utils = {};
 
+utils.checkStrongPassword = (password) => {
+    let pattern = /\d.*\d/;
+    if(utils.checkPattern(password, pattern) == -1) 
+        return utils.passwordErrorMessage('integers');
+    
+    pattern = /[A-Z].*[A-Z]/;
+    if(utils.checkPattern(password, pattern) == -1)
+        return utils.passwordErrorMessage('capital letters');
+
+    pattern = /[a-z].*[a-z]/;
+    if(utils.checkPattern(password, pattern) == -1)
+        return utils.passwordErrorMessage('lower case letters');
+
+    // checkign for 2 symbols
+    pattern = /[\W_].*[\W_]/;
+    if(utils.checkPattern(password, pattern) == -1)
+        return utils.passwordErrorMessage('symblos');
+}
+
 utils.checkPattern = (value, pattern) => {
     return value.search(pattern);
 }
 
-utils.checkStrongPassword = (password) => {
-    let pattern = /\d.*\d/;
-    if(utils.checkPattern(password, pattern) == -1) 
-        return false;
-    
+utils.passwordErrorMessage = (type) => {
+    return 'password should contain at least 2 ' + type;
 }
+
+console.log(utils.checkStrongPassword('123ASaa!@'))
