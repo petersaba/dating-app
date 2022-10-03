1087b700cc8502d4be78545fe0990ed792990dca
Peter Saba <petersaba001@gmail.com>
2022-10-03 14:59:08 +0300

    backend <> database: added create user table migration

--- backend/datingapp-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/datingapp-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->timestamp('date_of_birth');
+            $table->string('interested_in');
+            $table->string('gender');
+            $table->string('picture_url');
+            $table->longText('biography');
+            $table->string('location');
             $table->timestamps();
         });
     }
