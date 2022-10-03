3dd7d7a7eab2e90d5a47ac654854db786524b145
Peter Saba <petersaba001@gmail.com>
2022-10-03 23:45:04 +0300

    backend <> jwt: added jwt authentication

--- backend/datingapp-server/routes/api.php
+++ backend/datingapp-server/routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('test', [AuthController::class, 'test']);
 });
+
+
+//     Route::post('logout', [AuthController::class, 'logout']);
+//     Route::post('refresh', [AuthController::class, 'refresh']);
+//     Route::post('me', [AuthController::class, 'me']);
