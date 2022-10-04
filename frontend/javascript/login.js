0dd1276ba5d8802cffa74b8b847c94b18a6fcc37
Peter Saba <petersaba001@gmail.com>
2022-10-04 15:48:57 +0300

    frontend <> login: added condition to show error if the credentials are wrong

--- frontend/javascript/login.js
+++ frontend/javascript/login.js
@@ -1,6 +1,7 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const submit = document.getElementById('login');
+const error_field = document.querySelector('.error');
 
 submit.addEventListener('click', login);
 
@@ -11,6 +12,11 @@ async function login(event){
     data.append('password', password.value);
 
     const response = await utils.axiosPost('login', data);
+    if(!response){
+        utils.showError('Wrong credentials', error_field);
+        return;
+    }
+    utils.hideError(error_field);
     localStorage.setItem('token', response.data.access_token);
-    console.log(localStorage.getItem('token'));
+    console.log(response);
 }
