b636c3ea9f4dfac1767369dd9e5932db8ca5a7ab
Peter Saba <petersaba001@gmail.com>
2022-10-05 22:28:01 +0300

    frontend <> profile: added js file for profile page

---
+++ frontend/javascript/profile.js
@@ -0,0 +1,27 @@
+window.onload = async () => {
+    const response = await Promise.all([utils.axiosGet('userinfo', localStorage.getItem('token')),
+                                    utils.axiosGet('messages', localStorage.getItem('token'))]);
+
+    localStorage.setItem('user_info', JSON.stringify(response[0].data.message));
+    localStorage.setItem('messaged_users', JSON.stringify(response[1].data.message));
+
+    const card_container = document.querySelector('.profile-container');
+    const messages_container = document.querySelector('.messages')
+    const favorites_link = document.getElementById('favorites');
+    const profile_link = document.getElementById('profile');
+    const homepage_link = document.getElementById('homepage');
+    
+    
+    favorites_link.addEventListener('click', () => {
+        window.location.href = 'favorites.html';
+    });
+    profile_link.addEventListener('click', () => {
+        window.location.href = 'profile.html';
+    });
+    homepage_link.addEventListener('click', () => {
+        window.location.href = 'homepage.html';
+    });
+
+    // utils.fillCards(JSON.parse(localStorage.getItem('users')), card_container);
+    utils.fillMessages(JSON.parse(localStorage.getItem('messaged_users')), messages_container);
+}
